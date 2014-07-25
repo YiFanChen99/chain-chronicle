4477ed5a01b07a19fdb50e160328f22ad48bf190
RickyChen <pig211@gmail.com>
2014-07-25 23:11:21 +0800

    更新資料庫中三星騎士與戰士的資料 擴充武器的選擇，第二章多了拳銃狙

--- Static.py
+++ Static.py
@@ -8,7 +8,7 @@ PROFESSIONS = [u'戰士', u'騎士', u'弓手', u'法師', u'僧侶']
 RANKS = [5, 4, 3, 2, 1]
 RANKS_WHEN_DRAW_LOTS = [5, 4, 3]
 DRAW_LOTS_COST = [u'石抽', u'券抽', u'未記錄']
-WEAPONS = [u'斬', u'打', u'突', u'弓', u'魔', u'聖']
+WEAPONS = [u'斬', u'打', u'突', u'弓', u'魔', u'聖', u'拳', u'銃', u'狙']
 EXP_GROWN = [u'1000', u'750', u'500', u'300', u'LH']
 
 DATABASE = sqlite3.connect('ChainChronicle.sqlite')
