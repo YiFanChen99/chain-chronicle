ce7c7bc35391be85f9cc8643e809bbfdc174efd1
RickyChen <pig211@gmail.com>
2014-08-06 00:32:44 +0800

    增加九連抽的選項。 記錄九連抽的成果。

--- Static.py
+++ Static.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 PROFESSIONS = [u'戰士', u'騎士', u'弓手', u'法師', u'僧侶']
 RANKS = [5, 4, 3, 2, 1]
 RANKS_WHEN_DRAW_LOTS = [5, 4, 3]
-DRAW_LOTS_COST = [u'石抽', u'券抽', u'未記錄']
+DRAW_LOTS_COST = [u'石抽', u'券抽', u'未記錄', u'九連抽']
 WEAPONS = [u'斬', u'打', u'突', u'弓', u'魔', u'聖', u'拳', u'銃', u'狙']
 EXP_GROWN = [u'1000', u'750', u'500', u'300', u'LH']
 
