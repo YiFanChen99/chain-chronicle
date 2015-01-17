25d11a316efc251529a743f50a2e01895d52903f
RickyChen <pig211@gmail.com>
2015-01-18 04:50:16 +0800

    新增 TestingMain.py 以啟動測試。並增加三個測試案例。

---
+++ Test/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Ricky Chen'
+
+import CCGameDBTW
+import Character
---
+++ TestingMain.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Ricky Chen'
+
+from Test import *
+
+
+if __name__ == "__main__":
+    CCGameDBTW.run_all_test()
+    Character.run_all_test()
