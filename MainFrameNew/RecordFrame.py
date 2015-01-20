b08a149e8d245b8c0f8d00cd289790b0ad1e3d47
yifanchen <yifanchen@synology.com>
2015-01-20 16:09:27 +0800

    將 AdvancedDailyDroppedCanvas 抽出，便於之後調整。 細部調整。

--- MainFrameNew/RecordFrame.py
+++ MainFrameNew/RecordFrame.py
@@ -6,43 +6,51 @@ import codecs
 from BaseFrame import *
 from UIUtility.Button import ToggleButton
 
-RECORD_PATH = 'data\Record.txt'
-
 
 class RecordFrame(MainFrame):
     def __init__(self, master, **kwargs):
         MainFrame.__init__(self, master, **kwargs)
-        self._init_advanced_daily_dropped_fields()
-        self._init_advanced_daily_dropped_frame()
 
-    def _init_advanced_daily_dropped_fields(self):
+        self.advanced_daily_dropped = AdvancedDailyDroppedCanvas(self)
+        self.advanced_daily_dropped.place(x=10, y=10)
+
+
+class AdvancedDailyDroppedCanvas(Canvas):
+    RECORD_PATH = 'data\Record.txt'
+
+    def __init__(self, master, **kwargs):
+        Canvas.__init__(self, master, **kwargs)
+        self._init_fields()
+        self._init_frame()
+
+    def _init_fields(self):
         self.config_parser = configparser.ConfigParser()
-        self.config_parser.read(RECORD_PATH, "utf8")
+        self.config_parser.read(self.RECORD_PATH, "utf8")
         self.total = self.config_parser.getint('Advanced Daily', 'Total')
         self.dropped = self.config_parser.getint('Advanced Daily', 'Dropped')
 
         self.total_desc = StringVar()
         self.dropped_desc = StringVar()
         self.dropped_ratio_desc = StringVar()
-        self._update_advanced_daily_descs()
+        self._update_statistics()
 
-    def _init_advanced_daily_dropped_frame(self):
-        Label(self, text='上級曜日', width=8, font=(MS_JH, 14)).place(x=42, y=5)
-        self.third_button = ToggleButton(self, text='Third', width=7, font=(SCP, 11))
-        self.third_button.place(x=17 + 79 * 0, y=38)
-        self.fourth_button = ToggleButton(self, text='Fourth', width=7, font=(SCP, 11))
-        self.fourth_button.place(x=17 + 79 * 1, y=38)
+    def _init_frame(self):
+        Label(self, text='上級曜日', width=8, font=(MS_JH, 14)).place(x=43, y=5)
+        self.third_button = ToggleButton(self, text='Third', width=7, font=(SCP, 11), relief=RIDGE)
+        self.third_button.place(x=18 + 78 * 0, y=39)
+        self.fourth_button = ToggleButton(self, text='Fourth', width=7, font=(SCP, 11), relief=RIDGE)
+        self.fourth_button.place(x=18 + 78 * 1, y=39)
 
-        submit_button = Button(self, text='Submit', width=16, font=(SCP, 11))
-        submit_button.place(x=16, y=73)
+        submit_button = Button(self, text='Submit', width=15, font=(SCP, 11), relief=RIDGE)
+        submit_button.place(x=20, y=76)
         submit_button.bind('<Button-1>', self.submitting)
 
-        current_y = 119
-        Label(self, textvariable=self.total_desc, width=16, font=(SCP, 11)).place(x=15, y=current_y)
-        Label(self, textvariable=self.dropped_desc, width=16, font=(SCP, 11)).place(x=15, y=current_y + 20)
-        Label(self, textvariable=self.dropped_ratio_desc, width=16, font=(SCP, 11)).place(x=15, y=current_y + 40)
+        current_y = 114
+        Label(self, textvariable=self.total_desc, width=16, font=(SCP, 11)).place(x=16, y=current_y)
+        Label(self, textvariable=self.dropped_desc, width=16, font=(SCP, 11)).place(x=16, y=current_y + 20)
+        Label(self, textvariable=self.dropped_ratio_desc, width=16, font=(SCP, 11)).place(x=16, y=current_y + 40)
 
-    def _update_advanced_daily_descs(self):
+    def _update_statistics(self):
         self.total_desc.set(' Total  :  %4d ' % self.total)
         self.dropped_desc.set('Dropped :  %4d ' % self.dropped)
         self.dropped_ratio_desc.set(' Ratio  :  %.1f' % (self.dropped * 100.0 / self.total))
@@ -53,9 +61,9 @@ class RecordFrame(MainFrame):
         self.dropped += self.third_button.is_selected + self.fourth_button.is_selected
         self.config_parser.set('Advanced Daily', 'Total', str(self.total))
         self.config_parser.set('Advanced Daily', 'Dropped', str(self.dropped))
-        with codecs.open(RECORD_PATH, encoding="utf8", mode='wb') as data_file:
+        with codecs.open(self.RECORD_PATH, encoding="utf8", mode='wb') as data_file:
             self.config_parser.write(data_file)
 
         self.third_button.set_is_selected(False)
         self.fourth_button.set_is_selected(False)
-        self._update_advanced_daily_descs()
+        self._update_statistics()
